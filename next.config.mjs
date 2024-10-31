5f2441f66d87ecf1bcb26eb6fdc7df843513a326
Monika Swaraj <monikap@eduroam-226-28-58.laptops.usf.edu>
2024-10-31 14:41:08 -0400

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { images: {
+    remotePatterns: [
+      {
+        hostname: '*.googleusercontent.com'
+      },
+      {
+        hostname: 'linklist-files.s3.amazonaws.com',
+      },
+    ],
+  }
+}
+
+// module.exports = nextConfig
 
 export default nextConfig;
